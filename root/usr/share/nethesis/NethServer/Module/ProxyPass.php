63c7d43de244d58cc4adde1fc0be25323815a487
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-04-16 10:31:46 +0200

    Templates for Proxypass to vhost

--- root/usr/share/nethesis/NethServer/Module/ProxyPass.php
+++ root/usr/share/nethesis/NethServer/Module/ProxyPass.php
@@ -2,28 +2,27 @@
 namespace NethServer\Module;
 
 /*
- * Copyright (C) 2016 Nethesis S.r.l.
- * 
- * This script is part of NethServer.
- * 
- * NethServer is free software: you can redistribute it and/or modify
+ * Copyright (C) 2018 Nethesis Srl
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * NethServer is distributed in the hope that it will be useful,
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with NethServer.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Tabs and tables for Proxypass
+ *
+ * @author stephane de Labrusse <stephdl@de-labrusse.fr>
  */
 
-/**
- * Manage proxy pas
- */
-class ProxyPass extends \Nethgui\Controller\TableController
+class ProxyPass extends \Nethgui\Controller\TabsController
 {
 
     protected function initializeAttributes(\Nethgui\Module\ModuleAttributesInterface $base)
