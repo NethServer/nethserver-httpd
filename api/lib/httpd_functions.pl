3b1583d217e641cf639b3d49fca0f08099a033cd
Stephane de Labrusse <stephdl@de-labrusse.fr>
2019-05-02 18:29:46 +0200

    Functions API for nethserver-httpd

---
+++ api/lib/httpd_functions.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+#
+# Copyright (C) 2019 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+use strict;
+use warnings;
+use JSON;
+
+
+sub list_features
+{
+    my $features = {
+        dashboard => { installed => JSON::true, packages => ['nethserver-httpd'] },
+        virtualhosts => { installed => (-f '/etc/e-smith/db/vhosts/defaults/default/type') ? JSON::true : JSON::false, packages => ['nethserver-httpd-virtualhosts'] },
+        logs => { installed => JSON::true, packages => ['nethserver-httpd'] },
+        about => { installed => JSON::true, packages => ['nethserver-httpd'] },
+    };
+
+    return $features;
+}
+
+1;
