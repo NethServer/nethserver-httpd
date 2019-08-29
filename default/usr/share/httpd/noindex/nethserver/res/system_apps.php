37691d04d71e37c07734a77bae4c3ad86a4529c0
Andrea Leardini <andrea.leardini@nethesis.it>
2019-08-29 10:00:14 +0200

    config & api. httpd aliases and home page icons
     - Fixed httpd aliases
     - pinned_apps.json is no longer used, home page icons are now
    retrieved from /usr/share/cockpit/appName/logo.png

--- default/usr/share/httpd/noindex/nethserver/res/system_apps.php
+++ default/usr/share/httpd/noindex/nethserver/res/system_apps.php
@@ -1,6 +1,19 @@
 <?php
-    $hostname = gethostname();
-    $systemApps = shell_exec('echo \'{"action":"list","location":{"hostname":"' . $hostname . '"}}\' | /usr/bin/sudo /usr/libexec/nethserver/api/system-apps/read');
     header('Content-type: application/json');
-    echo $systemApps;
-?>
+    $cockpitPath = "/usr/share/cockpit/";
+    $cockpitInstalled = file_exists($cockpitPath);
+
+    if (!$cockpitInstalled) {
+        http_response_code(404);
+    } else {
+        $systemApps = shell_exec('echo \'{"action":"list","location":{"hostname":"' . $_SERVER['SERVER_NAME'] . '"}}\' | /usr/bin/sudo /usr/libexec/nethserver/api/system-apps/read');
+        $systemAppsJson = json_decode($systemApps, true);
+    
+        foreach ($systemAppsJson as &$app) {
+            $iconUrl = $cockpitPath . $app['id'] . '/' . $app['icon'];
+            $iconData = file_get_contents($iconUrl);
+            $app['iconBase64'] = base64_encode($iconData);
+        }
+        $systemApps = json_encode($systemAppsJson);
+        echo $systemApps;
+    }
