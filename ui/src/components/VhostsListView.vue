86383e621144086c428116b840cef0cae11521ce
Stephane de Labrusse <stephdl@de-labrusse.fr>
2019-11-20 11:40:10 +0100

    Show conventionnal PHP version

--- ui/src/components/VhostsListView.vue
+++ ui/src/components/VhostsListView.vue
@@ -96,7 +96,7 @@
               <div :class="[item.status === 'disabled' ? 'gray':'']">{{ item.Description }}</div>
             </div>
             <div class="list-group-item-text">
-              <div :class="[item.status === 'disabled' ? 'gray':'']">{{(item.PhpRhVersion ==='default')? 'PHP54' : item.PhpRhVersion.toUpperCase()}}</div>
+              <div :class="[item.status === 'disabled' ? 'gray':'']">{{ $t('virtualhost.php_version_' + item.PhpRhVersion) }}</div>
             </div>
           </div>
           <div class="list-view-pf-additional-info rules-info">
